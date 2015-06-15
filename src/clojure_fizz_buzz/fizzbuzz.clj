be88a2b20276d7f7fe52bbe11a919318257ba3b4
A Russo <abmmrusso@users.noreply.github.com>
2015-06-15 23:03:16 +0100

    First Fizz Buzz implementation

--- src/clojure_fizz_buzz/fizzbuzz.clj
+++ src/clojure_fizz_buzz/fizzbuzz.clj
@@ -1,2 +1,10 @@
-(ns clojure-fizz-buzz
-)
+(ns clojure-fizz-buzz.fizzbuzz)
+
+(defn fizzbuzz [finish]
+  (map (fn [number]
+          (cond
+            (zero? (mod number 15)) "FizzBuzz"
+            (zero? (mod number 3)) "Fizz"
+            (zero? (mod number 5)) "Buzz"
+            :else number))
+  (range 1 (inc finish))))
--- test/clojure_fizz_buzz/fizzbuzz_test.clj
+++ test/clojure_fizz_buzz/fizzbuzz_test.clj
@@ -1,3 +1,9 @@
 (ns clojure-fizz-buzz.fizzbuzz_test
   (:use clojure.test)
+  (:use clojure-fizz-buzz.fizzbuzz)
+)
+
+(deftest fizzbuzz_test
+  (is (=(seq '(1 2)) (fizzbuzz 2)))
+  (is (=(seq '(1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz" 11 "Fizz" 13 14 "FizzBuzz")) (fizzbuzz 15)))
 )
